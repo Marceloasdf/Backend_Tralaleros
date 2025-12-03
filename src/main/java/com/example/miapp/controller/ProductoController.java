a868e3eb4cbcfb706f6f0a645c3c7d2ed420b702
Marceloasdf <ma.novoac@duocuc.cl>
2025-12-02 22:36:21 -0300

    Swagger
    se añadio la dependencia esa de swagger

--- src/main/java/com/example/miapp/controller/ProductoController.java
+++ src/main/java/com/example/miapp/controller/ProductoController.java
@@ -48,6 +48,4 @@ public class ProductoController {
         productoRepository.deleteById(id);
     }
 
-    
-
 }
--- src/main/java/com/example/miapp/model/Producto.java
+++ src/main/java/com/example/miapp/model/Producto.java
@@ -13,22 +13,18 @@ public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private int price;
 
     // Mantenemos el nombre 'desc' para que el Frontend siga funcionando,
     // pero en la base de datos la columna se llamará 'descripcion_producto'
-    // para evitar el error de sintaxis
+    // pq si no el deploy no funciona xd
     @Column(name = "descripcion_producto")
     private String desc;
-    
-
     private String category;
 
     // imagen de la portada
     private String img; 
-
     // Array de imagenes que se usan en el detalle del producto, aunque siempre se usaba 1 imagen nomas xd  
     @ElementCollection
     @CollectionTable(name = "producto_imagenes", joinColumns = @JoinColumn(name = "producto_id"))
